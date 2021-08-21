cb7678705f3d4959e1e5f402898f4ef5d8b6ff18
gtumedei <gianni.tumedei@studio.unibo.it>
2021-08-21 10:48:27 +0200

    Add files

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useHead } from "@vueuse/head"
+
+useHead({
+  title: "Gianni Tumedei",
+  meta: [
+    { name: "description", content: "My portfolio website" },
+  ],
+})
+</script>
+
+<template>
+  <router-view />
+</template>
---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import "vue-global-api"
+import { ViteSSG } from "vite-ssg"
+import generatedRoutes from "virtual:generated-pages"
+import { setupLayouts } from "virtual:generated-layouts"
+import App from "./App.vue"
+
+// windicss layers
+import "virtual:windi-base.css"
+import "virtual:windi-components.css"
+// Custom styles here
+import "./styles/main.css"
+// windicss utilities should be the last style import
+import "virtual:windi-utilities.css"
+// windicss devtools support (dev only)
+import "virtual:windi-devtools"
+
+const routes = setupLayouts(generatedRoutes)
+
+export const createApp = ViteSSG(
+  App,
+  { routes },
+  (ctx) => {
+    Object.values(import.meta.globEager("./modules/*.ts")).map(i => i.install?.(ctx))
+  }
+)
