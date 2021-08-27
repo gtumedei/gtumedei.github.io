91703598a263dfca1d9e82e1f6480eb517d05a3e
gtumedei <gianni.tumedei@studio.unibo.it>
2021-08-27 19:40:45 +0200

    Lay down website layout

---
+++ src/views/home.vue
@@ -0,0 +1,120 @@
+<script lang="ts" setup>
+import { useDarkMode } from "@/hooks"
+
+const { toggleDark } = useDarkMode()
+</script>
+
+<template>
+  <div class="container text-primary px-6">
+
+    <div class="py-12 flex items-center gap-6">
+      <a href="#" class="text-lg no-underline inline-block mr-auto">Gianni Tumedei</a>
+      <mdi-translate class="text-secondary"/>
+      <mdi-theme-light-dark class="text-secondary" @click="() => toggleDark()"/>
+    </div>
+
+    <div class="
+      px-6 py-48 mb-18
+      bg-gradient-to-br from-[#FF8A80] to-[#FFD54F]
+      rounded-xl shadow-xl
+      flex flex-col justify-center items-center
+      text-primary-dark
+    ">
+      <h1 class="text-8xl">Hero</h1>
+    </div>
+
+    <div class="mb-18">
+      <h2 class="heading">Featured Projects</h2>
+
+      <div class="flex flex-col gap-12">
+
+        <div class="flex flex-col lg:flex-row lg:gap-24">
+          <div class="w-full lg:w-3/5 py-6 flex flex-col justify-center">
+            <h4 class="text-sm text-secondary font-bold uppercase mb-1">Type of Project</h4>
+            <h3 class="text-3xl mb-6">Project Name</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid nihil magni porro, veniam labore dolor officia eveniet adipisci quae repudiandae.</p>
+          </div>
+          <div class="w-full lg:w-2/5">
+            <div class="bg-[#666666] rounded-xl shadow-xl w-full h-72"></div>
+          </div>
+        </div>
+
+        <div class="flex flex-col lg:flex-row lg:gap-24">
+          <div class="w-full lg:w-3/5 py-6 flex flex-col justify-center order-1 lg:order-2">
+            <h4 class="text-sm text-secondary font-bold uppercase mb-1">Type of Project</h4>
+            <h3 class="text-3xl mb-6">Project Name</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid nihil magni porro, veniam labore dolor officia eveniet adipisci quae repudiandae.</p>
+          </div>
+          <div class="w-full lg:w-2/5 order-2 lg:order-1">
+            <div class="bg-[#666666] rounded-xl shadow-xl w-full h-72"></div>
+          </div>
+        </div>
+
+        <div class="flex flex-col lg:flex-row lg:gap-24">
+          <div class="w-full lg:w-3/5 py-6 flex flex-col justify-center">
+            <h4 class="text-sm text-secondary font-bold uppercase mb-1">Type of Project</h4>
+            <h3 class="text-3xl mb-6">Project Name</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid nihil magni porro, veniam labore dolor officia eveniet adipisci quae repudiandae.</p>
+          </div>
+          <div class="w-full lg:w-2/5">
+            <div class="bg-[#666666] rounded-xl shadow-xl w-full h-72"></div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="
+      p-6
+      bg-gradient-to-br from-[#A7FFEB] to-[#64B5F6]
+      rounded-xl shadow-xl
+      h-92
+      flex flex-col justify-center items-center
+      text-primary-dark
+    ">
+      <h2 class="heading mb-12 !after:bg-text-primary-dark">About me</h2>
+      <button class="
+        px-10 py-4 rounded-full !outline-none
+        inline-flex items-center gap-4
+        border-2 border-transparent
+        text-sm font-bold text-primary bg-primary shadow-md
+        transition-shadow transition-colors
+        hover:shadow-xl
+        focus:border-primary-dark focus:bg-primary-dark
+      ">
+        <span>Call to Action</span>
+        <mdi-open-in-new/>
+      </button>
+    </div>
+
+    <div class="py-12 flex justify-center gap-8 text-2xl text-secondary">
+      <mdi-email/>
+      <mdi-linkedin/>
+      <mdi-github/>
+      <mdi-map-marker/>
+    </div>
+
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+
+.heading { @apply
+  relative text-4xl text-center px-6 mb-18;
+
+  &::after {
+    content: "";
+    @apply
+      absolute -bottom-6 left-1/2 transform -translate-x-1/2
+      h-1 w-16 rounded-full bg-text-primary;
+  }
+}
+
+
+
+</style>
+
+<route lang="yaml">
+meta:
+  layout: empty
+</route>
--- windi.config.ts
+++ windi.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "windicss/helpers"
+import aspectRatio from "windicss/plugin/aspect-ratio"
 
 export default defineConfig({
   darkMode: "class",
   theme: {
 
     fontFamily: {
-      sans: ["Nunito", "sans-serif"],
+      sans: ["Lato", "sans-serif"],
       mono: ["'Fira Code'", "mono"]
     },
 
@@ -20,6 +21,28 @@ export default defineConfig({
           DEFAULT: "var(--color-dark-primary)",
           dark: "var(--color-dark-primary-dark)"
         },
+
+        primary: {
+          DEFAULT: "var(--color-primary)",
+          dark: "var(--color-primary-dark)"
+        },
+        accent: "var(--color-accent)",
+        text: {
+          primary: "var(--color-text-primary)",
+          secondary: "var(--color-text-secondary)",
+          faded: "var(--color-text-faded)",
+          "inverted-primary": "var(--color-text-inverted-primary)",
+          "inverted-secondary": "var(--color-text-inverted-secondary)",
+          "inverted-faded": "var(--color-text-inverted-faded)",
+          "primary-light": "var(--color-text-primary-light)",
+          "secondary-light": "var(--color-text-secondary-light)",
+          "faded-light": "var(--color-text-faded-light)",
+          "primary-dark": "var(--color-text-primary-dark)",
+          "secondary-dark": "var(--color-text-secondary-dark)",
+          "faded-dark": "var(--color-text-faded-dark)",
+        },
+
+        // Remove
         "accent-blue": {
           light: "var(--color-accent-blue-light)",
           dark: "var(--color-accent-blue-dark)"
@@ -35,13 +58,7 @@ export default defineConfig({
         "accent-pink": {
           light: "var(--color-accent-pink-light)",
           dark: "var(--color-accent-pink-dark)"
-        },
-
-        primary: {
-          DEFAULT: "var(--color-primary)",
-          dark: "var(--color-primary-dark)"
-        },
-        accent: "var(--color-accent)"
+        }
       },
 
       textColor: {
@@ -50,7 +67,13 @@ export default defineConfig({
         faded: "var(--color-text-faded)",
         "inverted-primary": "var(--color-text-inverted-primary)",
         "inverted-secondary": "var(--color-text-inverted-secondary)",
-        "inverted-faded": "var(--color-text-inverted-faded)"
+        "inverted-faded": "var(--color-text-inverted-faded)",
+        "primary-light": "var(--color-text-primary-light)",
+        "secondary-light": "var(--color-text-secondary-light)",
+        "faded-light": "var(--color-text-faded-light)",
+        "primary-dark": "var(--color-text-primary-dark)",
+        "secondary-dark": "var(--color-text-secondary-dark)",
+        "faded-dark": "var(--color-text-faded-dark)",
       },
 
       boxShadow: {
@@ -58,5 +81,8 @@ export default defineConfig({
         "btn-hover": "0 4px 8px var(--color-accent)"
       }
     }
-  }
+  },
+  plugins: [
+    aspectRatio
+  ]
 })
