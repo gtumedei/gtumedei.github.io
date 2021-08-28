126555315cc10493c87901655bb4b49b8451972d
gtumedei <gianni.tumedei@studio.unibo.it>
2021-08-28 15:18:28 +0200

    Remove unused packages

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,9 @@ import Pages from "vite-plugin-pages"
 import Layouts from "vite-plugin-vue-layouts"
 import ViteIcons, { ViteIconsResolver } from "vite-plugin-icons"
 import ViteComponents from "vite-plugin-components"
-import Markdown from "vite-plugin-md"
 import WindiCSS from "vite-plugin-windicss"
 import { VitePWA } from "vite-plugin-pwa"
 import VueI18n from "@intlify/vite-plugin-vue-i18n"
-import Prism from "markdown-it-prism"
-// @ts-expect-error missing types
-import LinkAttributes from "markdown-it-link-attributes"
-
-const markdownWrapperClasses = "prose prose-sm m-auto text-left"
 
 export default defineConfig({
   resolve: {
@@ -40,11 +34,8 @@ export default defineConfig({
 
     // https://github.com/antfu/vite-plugin-components
     ViteComponents({
-      // allow auto load markdown components under `./src/components/`
-      extensions: ["vue", "md"],
-
-      // allow auto import and register components used in markdown
-      customLoaderMatcher: id => id.endsWith(".md"),
+      // allow auto load components under `./src/components/`
+      extensions: ["vue"],
 
       // generate `components.d.ts` for ts support with Volar
       globalComponentsDeclaration: true,
@@ -64,27 +55,7 @@ export default defineConfig({
     }),
 
     // https://github.com/antfu/vite-plugin-windicss
-    WindiCSS({
-      safelist: markdownWrapperClasses
-    }),
-
-    // https://github.com/antfu/vite-plugin-md
-    // Don't need this? Try vitesse-lite: https://github.com/antfu/vitesse-lite
-    Markdown({
-      wrapperClasses: markdownWrapperClasses,
-      headEnabled: true,
-      markdownItSetup(md) {
-        // https://prismjs.com/
-        md.use(Prism)
-        md.use(LinkAttributes, {
-          pattern: /^https?:\/\//,
-          attrs: {
-            target: "_blank",
-            rel: "noopener"
-          }
-        })
-      }
-    }),
+    WindiCSS(),
 
     // https://github.com/antfu/vite-plugin-pwa
     VitePWA({
