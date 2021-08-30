a6fda1b4d0f82c0c5019eb1493b2fd0e96619ba2
gtumedei <gianni.tumedei@studio.unibo.it>
2021-08-30 16:52:12 +0200

    Improve AppFooter design

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -15,9 +15,38 @@ const toggleLocales = () => {
 
 <template>
   <footer class="container py-12 flex justify-center gap-8">
-    <a href="#" class="btn icon text-2xl !text-faded"><mdi-email/></a>
-    <a href="#" class="btn icon text-2xl !text-faded"><mdi-linkedin/></a>
-    <a href="#" class="btn icon text-2xl !text-faded"><mdi-github/></a>
-    <a href="#" class="btn icon text-2xl !text-faded"><mdi-map-marker/></a>
+    <a
+      href="#"
+      target="_blank"
+      class="footer-item btn icon"
+      title="Contact me"
+    ><mdi-email/></a>
+    <a
+      href="https://www.linkedin.com/in/gianni-tumedei-7171961b8/"
+      target="_blank"
+      class="footer-item btn icon"
+      title="Gianni Tumedei on LinkedIn"
+    ><mdi-linkedin/></a>
+    <a
+      href="https://github.com/gtumedei"
+      target="_blank"
+      class="footer-item btn icon"
+      title="gtumedei on GitHub"
+    ><mdi-github/></a>
+    <a
+      href="https://goo.gl/maps/knzcetCBj6cHLAAW7"
+      target="_blank"
+      class="footer-item btn icon"
+      title="Located in Cesena (IT)"
+    ><mdi-map-marker/></a>
   </footer>
 </template>
+
+<style lang="postcss" scoped>
+
+.footer-item { @apply
+  text-2xl !text-faded
+  !hover:text-base;
+}
+
+</style>
