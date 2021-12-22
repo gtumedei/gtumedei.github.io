48829a77138dd425e9185ceee16c0a17a65510eb
gtumedei <gianni.tumedei@studio.unibo.it>
2021-12-22 19:45:22 +0100

    Implement translate button action

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,18 +1,3 @@
-<!-- <script lang="ts" setup>
-import { useI18n } from "vue-i18n"
-import { useDarkMode } from "@/hooks"
-
-const { t, availableLocales, locale } = useI18n()
-const { isDark, toggleDark: toggle } = useDarkMode()
-
-const toggleDark = () => toggle()
-
-const toggleLocales = () => {
-  const locales = availableLocales
-  locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length]
-}
-</script> -->
-
 <template>
   <footer class="container py-12 flex justify-center gap-8">
     <a
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,30 @@
 <script lang="ts" setup>
+import { useI18n } from "vue-i18n"
 import { useDarkMode } from "@/hooks"
+
 const { isDark, toggleDark } = useDarkMode()
+
+const { t, availableLocales, locale } = useI18n()
+
+const toggleLocales = () => {
+  const locales = availableLocales
+  locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length]
+}
 </script>
 
 <template>
   <header class="container px-6 py-12 flex items-center gap-2">
     <router-link to="/" class="text-lg no-underline inline-block mr-auto">Gianni Tumedei</router-link>
-    <button class="btn icon" title="Change language">
+    <button
+      class="btn icon"
+      :title="t('ui.locale_button_title')"
+      @click="toggleLocales"
+    >
       <mdi-translate class="text-secondary"/>
     </button>
     <button
       class="btn icon"
-      :title="isDark ? 'Enable light theme' : 'Enable dark theme'"
+      :title="isDark ? t('ui.theme_button.light_title') : t('ui.theme_button.dark_title')"
       @click="() => toggleDark()"
     >
       <mdi-brightness-7 v-if="isDark" class="text-secondary"/>
