e280973cd1ae6c638dfb54fdd25b8cd830c81788
gtumedei <gianni.tumedei@studio.unibo.it>
2021-12-27 12:46:05 +0100

    Update WindiCSS

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -26,12 +26,3 @@ const { t, toggleLocales } = useI18n()
     </button>
   </header>
 </template>
-
-<style lang="postcss">
-
-.clip { @apply
-  fixed top-0 left-0 bottom-0 right-0
-  bg-primary transition transition-all;
-}
-
-</style>
