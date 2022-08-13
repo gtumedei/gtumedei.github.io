e183f77bf9fea323884d33e83ef83b1efbd4f908
gtumedei <giannitumedei@gmail.com>
2022-08-13 11:27:50 +0200

    Add NPM scripts for Supabase deployment; use Grammy to send the Telegram message

--- src/supabase/functions/send-message/index.ts
+++ src/supabase/functions/send-message/index.ts
@@ -1,3 +1,4 @@
+import { Bot, GrammyError, HttpError } from "https://deno.land/x/grammy@v1.10.1/mod.ts"
 import outdent from "https://deno.land/x/outdent@v0.8.0/mod.ts"
 import { z } from "https://deno.land/x/zod@v3.17.10/mod.ts"
 import { createFunction, SupabaseError } from "../_shared/functions.ts"
@@ -10,21 +11,28 @@ createFunction({
     message: z.string()
   }),
   handler: async (input) => {
-    const chatMessage = encodeURIComponent(outdent`
+    const message = outdent`
       Hello Sir,
-      *${input.name}* ✉️[${input.email}](mailto:${input.email}) contacted you via the gtumedei.github.io form.
+      *${input.name}* ✉️[${input.email}](${input.email}) contacted you via the gtumedei.github.io form.
 
 
       *${input.subject}*
 
       ${input.message}
-    `)
+    `
 
-    const res = await fetch(`https://api.telegram.org/bot${Deno.env.get("TELEGRAM_BOT_TOKEN")}/sendMessage?parse_mode=Markdown&chat_id=572114402&text=${chatMessage}`)
-    if (!res.ok) {
+    const bot = new Bot(Deno.env.get("TELEGRAM_BOT_TOKEN") ?? "")
+
+    try {
+      await bot.api.sendMessage(Deno.env.get("TELEGRAM_CHAT_ID") ?? "", message, { parse_mode: "Markdown" })
+      console.log("Message sent")
+    } catch (e) {
+      if (e instanceof GrammyError) console.error("Error in request:", e.description)
+      else if (e instanceof HttpError) console.error("Could not contact Telegram:", e)
+      else console.error("Unknown error:", e)
       throw new SupabaseError({
-        status: res.status,
-        message: await res.json()
+        status: 500,
+        message: "Failed to send the message"
       })
     }
   }
