fa571d1217976d868dbf1e269162fb7d5cf9250a
gtumedei <giannitumedei@gmail.com>
2022-10-28 15:43:06 +0200

    Update dependencies; add typecheck script

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js"
 const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
 const supabaseKey = import.meta.env.PUBLIC_SUPABASE_KEY
 
-const supabase = createClient(supabaseUrl, supabaseKey, { shouldThrowOnError: true })
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 export type Message = {
   name: string
