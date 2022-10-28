10f1ce276d27a6942ebf60cea727b0cebefb8a10
gtumedei <giannitumedei@gmail.com>
2022-10-28 15:14:18 +0200

    Create pages for all games

---
+++ src/components/games/memory-game.tsx
@@ -0,0 +1,67 @@
+import { Component, createSignal, For, mergeProps } from "solid-js"
+import { BoardContainer } from "~/components/games/ui"
+import tooltip from "~/lib/tooltip"
+import MdiCog from "~icons/mdi/cog"
+import MdiEllipse from "~icons/mdi/ellipse"
+import MdiTeddyBear from "~icons/mdi/teddy-bear"
+
+tooltip
+
+const Toolbar = () => {
+  return (
+    <div class="w-full flex items-center p-2 rounded-xl border">
+      <p class="ml-3 flex-grow">99 moves</p>
+      <button class="btn icon" use:tooltip={[() => "Game Settings", "top"]}>
+        <MdiCog class="text-lg" />
+      </button>
+    </div>
+  )
+}
+
+const Card: Component<{ icon: string }> = (props) => {
+  const [face,  setFace] = createSignal<"up" | "down">("down")
+
+  return (
+    <button
+      class={`
+        aspect-square bg-primary-dark rounded-xl p-2 border hover:border-accent transition-colors
+        ${face() == "up" ? "!border-accent-20" : ""}
+      `}
+      onClick={() => setFace(f => f == "up" ? "down" : "up")}
+    >
+      {face() == "up"
+        ? <div class="h-full w-full flex">
+            <MdiTeddyBear class="text-6xl text-accent m-auto" />
+          </div>
+        : <div class="h-full w-full flex rounded-lg border border-dashed">
+            <MdiEllipse class="text-4xl opacity-20 rotate-90 m-auto" />
+          </div>
+      }
+    </button>
+  )
+}
+
+const Board: Component<{ cardCount?: number }> = (props) => {
+  const allProps = mergeProps({ cardCount: 16 }, props)
+
+  return (
+    <div class="grid grid-cols-4 gap-3">
+      <For each={new Array(allProps.cardCount)}>{() =>
+        <Card icon="a" />
+      }</For>
+    </div>
+  )
+}
+
+const MemoryGame: Component = () => {
+  return (
+    <>
+      <BoardContainer>
+        <Board />
+      </BoardContainer>
+      <Toolbar />
+    </>
+  )
+}
+
+export default MemoryGame
