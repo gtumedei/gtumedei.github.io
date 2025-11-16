6de108549e0f0929713a2b3c4bfe2cca4c9acc4e
gtumedei <giannitumedei@gmail.com>
2025-11-16 10:13:55 +0000

    New page heading icon style

--- src/components/page-heading-icon.tsx
+++ src/components/page-heading-icon.tsx
@@ -8,14 +8,12 @@ const PageHeadingIcon: ParentComponent<ComponentProps<"div">> = (props) => {
     <div
       {...divProps}
       class={cn(
-        "inline-flex rounded-full border border-base-300/70 p-3 relative mb-3 -mt-3 -ml-3",
+        "w-20 h-20 inline-flex bg-base-200/70 backdrop-blur-xs p-2 rounded-full border border-on-base/10 shadow shadow-black/3 mb-8",
         localProps.class
       )}
     >
-      <div class="inline-flex bg-base-200/50 rounded-full border border-base-300 p-3">
-        <div class="h-12 w-12 inline-flex justify-center items-center bg-base-300 text-accent text-lg rounded-full p-3">
-          {localProps.children}
-        </div>
+      <div class="w-full h-full flex justify-center items-center bg-base-300 text-accent text-lg rounded-full">
+        {localProps.children}
       </div>
     </div>
   )
