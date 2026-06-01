4e240483998a256cee7a33f1c5affe7f9bec3659
gtumedei <giannitumedei@gmail.com>
2026-06-01 15:39:47 +0200

    Raised button design tweaks

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,7 +16,7 @@ export const button = tv({
         "border-transparent bg-transparent text-on-base hover:bg-on-base/5 focus-visible:border-on-base/20 focus-visible:ring-on-base/10 [&:where(:disabled,[data-disabled])]:bg-transparent! [&:where(:disabled,[data-disabled])]:text-on-base/50!",
       link: "border-transparent h-min! px-0!",
       raised:
-        "bg-base-100 dark:bg-base-200 hover:bg-base-200 rounded-full! border border-on-base/10 hover:border-on-base/20 shadow-md shadow-black/3 focus-visible:ring-on-base/10",
+        "bg-base-200 hover:bg-base-200! rounded-full! border border-on-base/10 hover:border-on-base/20 shadow-md shadow-black/3 focus-visible:ring-on-base/10",
     },
     theme: {
       neutral: "",
@@ -91,7 +91,7 @@ export const Button = (props: ButtonProps) => {
   const [variantProps, classProp, buttonProps] = splitProps(
     props,
     ["variant", "theme", "size", "shape"],
-    ["class"]
+    ["class"],
   )
   return <ark.button class={cn(button(variantProps), classProp.class)} {...buttonProps} />
 }
